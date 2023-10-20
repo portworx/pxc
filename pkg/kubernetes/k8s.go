bfec06b489921cdfc4d25a3915e2b675b61bcb6a
Luis Pabón <lpabon@purestorage.com>
2023-10-20 11:47:21 -0400

    Import client-go auth plugins
    Closes #198
    
    Signed-off-by: Luis Pabón <lpabon@purestorage.com>

--- pkg/kubernetes/k8s.go
+++ pkg/kubernetes/k8s.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,6 +30,12 @@ import (
 	kclikube "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+
+	// Import client auth
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/exec"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 var (
